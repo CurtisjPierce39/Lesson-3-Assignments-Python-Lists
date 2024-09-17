c6dd5a0fb87f9047770a1d1efc0723696e89120c
Curtis J Pierce <Curtisjpierce39@gmail.com>
2024-09-16 19:19:31 -0700

    add initial description

--- Advanced List Methods and Identity Operators.py
+++ Advanced List Methods and Identity Operators.py
@@ -2,7 +2,7 @@
 submitted = ["Alice", "Bob", "Charlie", "David"]
 attended = ["Charlie", "Eve", "Alice", "Frank"]
 
-if "Alice" in submitted and attended:
+if "Alice" in submitted and "Alice" in attended:
     print("Alice attended class and submitted her assignment!")
 else:
     print("Alice did not attend class and did not submit her assignment!")
